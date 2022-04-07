bc072608c7cf6c46a95600d1e51696761c4b4d39
j-hm <thinkalonej@gmail.com>
2022-04-07 19:07:33 +0900

    create react-calendar

--- src/comp/TomoEvent.jsx
+++ src/comp/TomoEvent.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 function TomoEvent() {
-  return <div>TomoEvent</div>;
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+        <p>forTomo</p>
+        <button>+</button>
+      </div>
+      <Calendar calendarType={"US"} onChange={setDate} value={date} />
+      <input value={date.toLocaleDateString()} readOnly />
+    </>
+  );
 }
 
 export default TomoEvent;
